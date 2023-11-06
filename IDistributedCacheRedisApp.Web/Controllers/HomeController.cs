dd8708571e411682bbc4f3a68ab5b3aad43ecc6f
Anıl Düz <96021036+Anilduz@users.noreply.github.com>
2023-11-06 09:50:25 +0300

    Redis project load commit

---
+++ IDistributedCacheRedisApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IDistributedCache _distributedCache;
+
+        public HomeController(ILogger<HomeController> logger, IDistributedCache distributedCache)
+        {
+            _logger = logger;
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddSeconds(30);
+            //_distributedCache.SetString("isim", "zeynep", cacheEntryOptions);
+             
+            //var name = _distributedCache.GetString("isim");
+            //ViewBag.name = name;
+
+
+            //_distributedCache.Remove("isim");
+
+
+
+            Product product = new Product { Id = 2, Name = "iPhone 13 mini", Price = 199 };
+
+            string jsonprod = JsonConvert.SerializeObject(product);
+
+            _distributedCache.SetStringAsync("product:2", jsonprod);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
